2505cf3aa7d8b20912621f73e66de0a716f14170
Austin Bolles <austinbolles@bsuadmins-iMac.local>
2019-03-21 20:09:10 -0400

    setting up Login database

---
+++ HTML/Test.php
@@ -0,0 +1,14 @@
+<?php
+    include_once 'connect.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test SQL Site</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+
+</body>
+</html>
---
+++ HTML/connect.php
@@ -0,0 +1,10 @@
+<?php
+
+$Servername = "csums.dhcp.bsu.edu";
+$Username = "UTS";
+$Password = "iamroot";
+$Name = "mess_test";
+
+$conn = mysql_connet($Servername, $Username, $Password, $Name);
+
+?>
