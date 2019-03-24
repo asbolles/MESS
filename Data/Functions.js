8124bab2ffd92b7eb342d26008963195b9351047
snoe9 <sfstull@bsu.edu>
2019-03-24 03:39:54 -0400

    Master Schedule Full view Fix

--- Data/Functions.js
+++ Data/Functions.js
@@ -23,80 +23,82 @@ for(i=0;i<boxlist.length;i++){
 }//end of changepage
 
 function createFull(){
-TestFunction();
-}
-
-
-function TestFunction(){
-    var div = document.getElementById("tableLocation");
-    div.innerHTML += "test";
-    //div.innerHTML += "<table class='table'><td class='head'>Assistant Shift Schedule for MWF</td>";
+   
+    //string += " but not this"
+    //FullMWF();
+    var div = document.getElementById('tableLocation');
+        div.innerHTML += FullMWF()+"<p></p>"+FullTR();
+        
 }
 
 function FullMWF(){
-    var div = document.getElementById("tableLocation");
-    div.innerHTML += "<table class='table'><td class='head'>Assistant Shift Schedule for MWF</td>";
+    var div = "";
+    div += "<table class='table'><td class='head'>Assistant Shift Schedule for MWF</td>";
         var titles =[];
         listofSa.forEach(function(sa){
             titles.push(sa.name);
         })
         
         var times=["8:00am - 8:50am","9:00am - 9:50am","10:00am - 10:50am","11:00am - 11:50am","12:00pm - 12:50pm","1:00pm - 1:50pm","2:00pm - 2:50pm","3:00pm - 3:50pm","4:00pm - 4:50pm","5:00pm - 6:15pm"]
-        var daysMWF=["M","W","F",]
-        div.innerHTML += "<tr>";
-        div.innerHTML += "<td class='test'>Assistants</td>")
+        var daysMWF=["M","W","F",];
+        div += "<tr>";
+        div += "<td class='test'>Assistants</td>";
         titles.forEach(function(title){
-            div.innerHTML +="<td colspan='3' class='test'>"+title+"</td>";
+            div +="<td colspan='3' class='test'>"+title+"</td>";
         })
-        div.innerHTML +="<tr>";
-            div.innerHTML +="<tr class='test'><td>Day of the Week</<td>";
+        div +="<tr>";
+            div +="<tr class='test'><td>Day of the Week</<td>";
                 titles.forEach(function(title){
                     daysMWF.forEach(function(day){
-                    div.innerHTML +="<td>"+day+"</td>";
+                    div +="<td>"+day+"</td>";
                 })    
             }) 
     
-        div.innerHTML +="</tr>";
+        div +="</tr>";
             var counter=0;
             var classesCounter=0;
             for(i=0;i<MWFnumber;i++){
-                div.innerHTML +=("<tr>");
-                div.innerHTML +="<td class='days'>"+times[counter]+"</td>";
-                CreateChartMWF(counter,classesCounter);//breaks
+                div +="<tr>";
+                div +="<td class='days'>"+times[counter]+"</td>";
+                div +=CreateChartMWF(counter,classesCounter);
                 counter++;
                 classesCounter=counter*3;
-                div.innerHTML +="</tr>"; 
+                div +="</tr>"; 
             }
-    div.innerHTML +="</table>"
+    div +="</table>"
+    return div;
 }
 
 
 function CreateChartMWF(counter,classesCounter){
+    var div ="";
     listofSa.forEach(function(sa){
     x=0;
     while(x<3){//the times 3 is for MWF
         var greenAV=sa.green.split(",");
         var yellowAV= sa.yellow.split(",");
         if(greenAV.includes(MWFclasses[classesCounter])){
-            document.write("<td class='box green'>Green</td>");
+            div +="<td class='box green'>Green</td>";
         }
         else if(yellowAV.includes(MWFclasses[classesCounter])){
-            document.write("<td class='box yellow'>Yellow</td>");
+            div +="<td class='box yellow'>Yellow</td>";
         }
         else {
-            document.write("<td class='box red'>Red</td>");
+            div +="<td class='box red'>Red</td>";
         }
         x++;
         classesCounter++;
     }//end of while 
     classesCounter=counter*3;
     })
+    return div;
 }//end of CreateChartMWF
 
 
 
 function FullTR(){
-    document.write("<table class='table'> <td class='head'> Assistant Shift Schedule for TR </td>")
+    var div = "";
+    div +="<table class='table'> <td class='head'> Assistant Shift Schedule for TR </td>"
     var titles =[];
     listofSa.forEach(function(sa){
         titles.push(sa.name);
@@ -104,56 +106,57 @@ function FullTR(){
     
     var times=["8:00am - 8:50am","9:00am - 9:50am","10:00am - 10:50am","11:00am - 11:50am","12:00pm - 12:50pm","1:00pm - 1:50pm","2:00pm - 2:50pm","3:00pm - 3:50pm","4:00pm - 4:50pm","5:00pm - 6:15pm"]
     var daysTR=["T","R"]
-    document.write("<tr>");
-    document.write("<td class='test'>Assistants</td>")
+    div +="<tr><td class='test'>Assistants</td>";
     titles.forEach(function(title){
-        document.write("<td colspan='2' class='test'>"+title+"</td>");
+        div +="<td colspan='2' class='test'>"+title+"</td>";
     })
-    document.write("<tr>");
-        document.write("<tr class='test'><td>Day of the Week</<td>");
+    div +="<tr>";
+        div +="<tr class='test'><td>Day of the Week</<td>";
             titles.forEach(function(title){
                 daysTR.forEach(function(day){
-                document.write("<td>"+day+"</td>");
+                div +="<td>"+day+"</td>";
             })    
         }) 
 
-    document.write("</tr>");
+    div +="</tr>";
         var counter=0;
         var classesCounter=0;
         for(i=0;i<TRnumber;i++){
-            document.write("<tr>")
-            document.write("<td class='days'>"+times[counter]+"</td>");
-            CreateChartTR(counter,classesCounter);
+            div +="<tr>";
+            div +="<td class='days'>"+times[counter]+"</td>";
+            div += CreateChartTR(counter,classesCounter);
             counter++;
             classesCounter=counter*2;
-            document.write("</tr>"); 
+            div +="</tr>"; 
         }
-        document.write("</table>")
-}
+        div +="</table>";
+        return div;
+}//end of FullTR
 
 
 
 function CreateChartTR(counter,classesCounter){
+    div ="";
     listofSa.forEach(function(sa){
         x=0;
         while(x<2){//the times 2 is for TR
         var greenAV=sa.green.split(",");
         var yellowAV= sa.yellow.split(",");
         if(greenAV.includes(TRclasses[classesCounter])){
-            document.write("<td class='box green'>Green</td>");
+            div +="<td class='box green'>Green</td>";
         }
         else if(yellowAV.includes(TRclasses[classesCounter])){
-            document.write("<td class='box yellow'>Yellow</td>");
+            div +="<td class='box yellow'>Yellow</td>";
         }
         else {
-            document.write("<td class='box red'>Red</td>");
+            div +="<td class='box red'>Red</td>";
         }
         x++;
         classesCounter++;
     }//end of while 
     classesCounter=counter*2;
 })
-   
+   return div;
 }//end of CreateChartTR
 
 
