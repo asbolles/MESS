0d8e45579839252835820f205f48c93f573571d6
snoe9 <sfstull@bsu.edu>
2019-04-01 03:41:14 -0400

    Avail sheet work on

---
+++ JavaScript/Availibility_functions.js
@@ -0,0 +1,50 @@
+function changePage(){
+    var boxlist=document.getElementsByClassName("box");
+    for(i=0;i<boxlist.length;i++){
+        boxlist[i].onclick=function(e){
+            if(e.target.classList.contains("red")){               
+                e.target.classList.replace("red","green");
+               e.target.style.color= "green";
+               e.target.style.backgroundColor = "green";           
+            }
+            else if(e.target.classList.contains("yellow")){
+                e.target.classList.replace("yellow","red");
+                e.target.style.color= "red";
+                e.target.style.backgroundColor = "red";           
+            }
+            else if(e.target.classList.contains("green")){
+                e.target.classList.replace("green","yellow");
+                e.target.style.color = "yellow";
+                e.target.style.backgroundColor = "yellow";           
+            }
+        }
+    }
+    }//end of changepage
+function submitAvail(min){
+   min=parseInt(min,10);
+    var green=[];
+    var yellow=[];
+    var boxlist=document.getElementsByClassName("box");
+   for(var i=0; i<boxlist.length; i++){
+        if(boxlist[i].classList.contains("green")){
+            green.push(boxlist[i].id);
+        }
+        else if(boxlist[i].classList.contains("yellow")){
+            yellow.push(boxlist[i].id);
+        }
+    }
+    if (min>green.length+yellow.length){
+        green.length=0;
+        yellow.length=0;
+        alert("You have requested more hours than are available. Please select more hours.");
+    }
+    else{
+        var returned =[green, yellow];
+        return returned;
+    }
+    //austin the greenAvail=green and yellowAvail=yellow;
+}
+function compileInfo(min, vet1, vet2){
+    var availInfo= submitAvail(min);
+    alert("test");    
+}
--- JavaScript/Functions.js
+++
@@ -1,3 +0,0 @@
-function test(){
-    document.write("<hr>this is not a drill</hr>")
-}
