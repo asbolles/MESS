21067dc237a13da63baf626ea7b341a2d1906874
austinbolles <asbolles@bsu.edu>
2019-04-02 20:18:09 -0400

    Changed database connection

--- HTML/connect.php
+++ HTML/connect.php
@@ -2,7 +2,7 @@
 DEFINE ('DB_USER', 'MESS');
 DEFINE ('DB_PASD', 'mess');
 DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
-DEFINE ('DB_NAME', 'mess_test');
+DEFINE ('DB_NAME', 'mess');
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
 if (!$link) {
     die('error connecting to database');
