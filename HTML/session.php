d92a6244669ff1ea1ef17644e21e8bafffe5a8c9
austinbolles <asbolles@bsu.edu>
2019-04-13 22:17:53 -0400

    error fix

--- HTML/session.php
+++ HTML/session.php
@@ -6,6 +6,8 @@ $user_check = $_SESSION['login_user'];
 
 $ses_sql = mysqli_query($link,"select username from admin where username = '$user_check' ");
 
+$result = $link->query($query) or die('error getting name');
+
 $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
 
 $login_session = $row['username'];
