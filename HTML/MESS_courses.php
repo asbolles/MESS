78af88b552cf5f19d2f9074ac67fb3b902be2677
austinbolles <asbolles@bsu.edu>
2019-04-22 22:09:03 -0400

    duplicate define

--- HTML/MESS_courses.php
+++ HTML/MESS_courses.php
@@ -142,10 +142,10 @@
 
 
     <?php
-DEFINE ('DB_USER', 'MESS');
-DEFINE ('DB_PASD', 'mess');
-DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
-DEFINE ('DB_NAME', 'mess');
+//DEFINE ('DB_USER', 'MESS');
+//DEFINE ('DB_PASD', 'mess');
+//DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
+//DEFINE ('DB_NAME', 'mess');
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
 if (!$link) {
     die('error connecting to database');
