fcc944b9028ff12fbc35e638044d652a7ac1461b
austinbolles <asbolles@bsu.edu>
2019-04-22 22:40:32 -0400

    db fix

--- HTML/MESS_courses.php
+++ HTML/MESS_courses.php
@@ -110,23 +110,23 @@
                     <td>
                         <form method = post action = remove_course.php>
                         <?php
-                DEFINE ('DB_USER', 'MESS');
-                DEFINE ('DB_PASD', 'mess');
-                DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
-                DEFINE ('DB_NAME', 'mess');
-                $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
-                if (!$link) {
-                    die('error connecting to database');
-                }
-                $sql = "SELECT ID FROM courses;";
-                $result = $link->query($sql) or die("error getting data");
-                echo "<br>Select Course ID to remove: <select id = cid name ='cid'>";
-                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                    echo "<option value='" . $row[ID] ."'>" . $row[ID] ."</option>";
-                }
-                echo "</select>";
-                mysqli_close($link);
-                ?>
+                        DEFINE ('DB_USER', 'MESS');
+                        DEFINE ('DB_PASD', 'mess');
+                        DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
+                        DEFINE ('DB_NAME', 'mess');
+                        $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
+                        if (!$link) {
+                            die('error connecting to database');
+                        }
+                        $sql = "SELECT ID FROM courses;";
+                        $result = $link->query($sql) or die("error getting data");
+                        echo "<br>Select Course ID to remove: <select id = cid name ='cid'>";
+                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                            echo "<option value='" . $row[ID] ."'>" . $row[ID] ."</option>";
+                        }
+                        echo "</select>";
+                        mysqli_close($link);
+                        ?>
                         <br><input type = "submit"value="Delete Class">
                         
                     <td>
@@ -142,10 +142,6 @@
 
 
     <?php
-//DEFINE ('DB_USER', 'MESS');
-//DEFINE ('DB_PASD', 'mess');
-//DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
-//DEFINE ('DB_NAME', 'mess');
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
 if (!$link) {
     die('error connecting to database');
--- HTML/databasepull.php
+++ HTML/databasepull.php
@@ -3,24 +3,19 @@
 
 DEFINE ('DB_USER', 'MESS');
 DEFINE ('DB_PASD', 'mess');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'mess_test');
+DEFINE ('DB_HOST', 'csums.dhcp.bsu.edu');
+DEFINE ('DB_NAME', 'mess');
 $link = mysqli_connect(DB_HOST, DB_USER, DB_PASD, DB_NAME);
 if (!$link) {
     die('error connecting to database');
 }
-echo 'connection established <br>';
-
-$sql2 = "SELECT fname FROM users;";
-
-$result2 = $link->query($sql2) or die("error getting data");
-echo "<select name='name'>";
-
-while ($row2 = mysqli_fetch_array($result2, MYSQLI_ASSOC)){
-    echo "<option value='" . $row['fname'] ."'>" . $row['fname'] ."</option>";
-   
+$sql = "SELECT ID FROM courses;";
+$result = $link->query($sql) or die("error getting data");
+echo "<br>Select Course ID to remove: <select id = cid name ='cid'>";
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+    echo "<option value='" . $row[ID] ."'>" . $row[ID] ."</option>";
 }
- echo "</select>";
+echo "</select>";
 mysqli_close($link);
 
 
