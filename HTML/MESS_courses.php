21833e64169f32317284020e03cbdb0ffc35f3c2
snoe9 <sfstull@bsu.edu>
2019-04-25 02:05:33 -0400

    courses change

--- HTML/MESS_courses.php
+++ HTML/MESS_courses.php
@@ -122,7 +122,7 @@
                         $result = $link->query($sql) or die("error getting data");
                         echo "<br>Select Course ID to remove: <select id = cid name ='cid'>";
                         while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-                            echo "<option value='" . $row[ID] ."'>" . $row[ID] ."</option>";
+                            echo "<option value='" . $row["ID"] ."'>" . $row["ID"] ."</option>";
                         }
                         echo "</select>";
                         mysqli_close($link);
